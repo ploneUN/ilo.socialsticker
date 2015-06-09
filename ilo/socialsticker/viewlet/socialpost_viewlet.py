50a0cf9768d214a6aa6a376aa1d0b370624ca015
glenn <glenn@glenn-Lenovo-G480>
2015-06-09 14:46:17 +0800

    revised viewlet python code

--- ilo/socialsticker/viewlet/socialpost_viewlet.py
+++ ilo/socialsticker/viewlet/socialpost_viewlet.py
@@ -1,25 +1,35 @@
-from five import grok
+#from five import grok
+from Acquisition import aq_inner
 from plone.app.layout.viewlets.interfaces import IAboveContent
 from Products.CMFCore.utils import getToolByName
-from ilo.pledge.content.pledge import IPledge
+#from ilo.pledge.content.pledge import IPledge
+from plone.app.layout.viewlets import common as base
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 
-grok.templatedir('templates')
+#grok.templatedir('templates')
 
-class socialpost_viewlet(grok.Viewlet):
-        grok.context(IPledge)
-        grok.require('zope2.View')
-        grok.template('socialpost_viewlet')
-        grok.viewletmanager(IAboveContent)
+
+class socialpost_viewlet(base.ViewletBase):
+    #grok.context(IPledge)
+    #grok.require('zope2.View')
+    #grok.template('socialpost_viewlet')
+    #grok.viewletmanager(IAboveContent)
+    
+    index = ViewPageTemplateFile('templates/socialpost_viewlet.pt')
+    
+
+    @property
+    def catalog(self):
+        return getToolByName(self.context, 'portal_catalog')
+
+    def contents(self):
+        context = self.context
+        catalog = self.catalog
+        path = '/'.join(context.aq_parent.getPhysicalPath())
+        brains = catalog.unrestrictedSearchResults(path={'query': path, 'depth' : 1}, portal_type='ilo.socialsticker.sticker',review_state='published')
         
-        @property
-        def catalog(self):
-            return getToolByName(self.context, 'portal_catalog')
-        
-    	def contents(self):
-	    	context = self.context
-	    	catalog = self.catalog
-	    	path = '/'.join(context.aq_parent.getPhysicalPath())
-	    	brains = catalog.unrestrictedSearchResults(path={'query': path, 'depth' : 1}, portal_type='ilo.socialsticker.sticker',review_state='published')
-	    	return brains
+        return brains
+
+
 
 
